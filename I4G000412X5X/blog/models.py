365164e74aa995f2268faf7b83ac9d83f756ed90
Oluwakemi Owolabi <kemiowolabi90@yahoo.com>
2022-06-21 15:25:36 +0100

    updated

--- I4G000412X5X/blog/models.py
+++ I4G000412X5X/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    created_on = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Meta:
+    ordering = ('-created_on')
+
+def __str__(self):
+    return self.title
